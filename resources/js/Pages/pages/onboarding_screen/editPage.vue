5e1901ae76e0418dd19db373e0be239172a0ae38
mohamed magdy saber <60584604+momagdy194@users.noreply.github.com>
2026-03-12 06:02:01 +0200

    Update sidebar images and adjust layout in onboarding screen edit page for improved responsiveness

--- resources/js/Pages/pages/onboarding_screen/editPage.vue
+++ resources/js/Pages/pages/onboarding_screen/editPage.vue
@@ -201,7 +201,7 @@ export default {
               </div>
           </BCardHeader>
       </BCard>
-      <BCol lg="6">
+      <BCol v-if="false" lg="6">
         <BCard no-body id="tasksList">
           <BCardHeader class="border-0"><h5>{{$t("mobile_view")}}</h5></BCardHeader>
           <BCardBody class="border border-dashed border-end-0 border-start-0">
@@ -224,7 +224,7 @@ export default {
           </BCardBody>
         </BCard>
       </BCol>
-      <BCol lg="6">
+      <BCol lg="12">
         <BCard no-body id="tasksList">
           <BCardHeader class="border-0"></BCardHeader>
           <BCardBody class="border border-dashed border-end-0 border-start-0">
